f6c43bcc7a2abe80b56ec95f4a8034b12147e17f
Anhtt <tuganhhy@gmail.com>
2023-11-10 15:25:09 +0700

    anhtt

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 window.onload = function () {
-  var vueInstance = new Vue({
-      el: '#login',
-      data: {
-          username: '',
-          password: '',
-          error: ''
-      },
-      methods: {
-          login(){
-            if (this.username == 'tunganh' && this.password == '12345') {
-              this.error = 'Login successful';
-            } else {
-              this.error = 'Wrong username or password';
-            }
-          }
-        }
-  });
+var vueInstance = new Vue({
+  el: '#login',
+  data: {
+    username: '',
+    password: '',
+    error: ''
+  },
+  methods: {
+    async login() {
+        // if (this.username == 'tunganh' && this.password == '12345') {
+        //   this.error = 'Login successful';
+        // } else {
+        //   this.error = 'Wrong username or password';
+        // }
+        const article = { "Username": username, "Password": password };
+        const response = await axios.post("https://localhost:44300/api/accounts/login", article);
+        console.log(response);
+      }
+    }    
+});
 }
